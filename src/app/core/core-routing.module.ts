3d129016222e630a763ac7bcb6ea7e13ce4ff787
pradeep <pradeepkumar.padakanti@manomay.biz>
2022-06-15 19:17:35 +0530

    web_headers_page

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -2,15 +2,18 @@ import { ResetPasswordComponent } from './components/reset-password/reset-passwo
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { HeaderFooterComponent } from './components/header-footer/header-footer.component';
 
 
 
 const routes: Routes = [
     {path:'login',component:LoginComponent},
   {
-      path:'',component:LoginComponent
+      path:'login',component:LoginComponent
   } ,
-  {path:'resetpass',component:ResetPasswordComponent}
+  {path:'resetpass',component:ResetPasswordComponent},
+  {path:'header_footer',component:HeaderFooterComponent},
+  {path:'assitance_req_form',component:HeaderFooterComponent},
   
 ];
 
