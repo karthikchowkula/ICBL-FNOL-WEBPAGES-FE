b9d765cde0fca8e7fdf0318046fe5ada4c39955e
chowkula karthik <karthikchowkula@manomay.biz>
2022-06-22 16:57:19 +0530

    admin changes

---
+++ src/app/core/components/admin/deleteuser/deleteuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deleteuser',
+  templateUrl: './deleteuser.component.html',
+  styleUrls: ['./deleteuser.component.scss']
+})
+export class DeleteuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
